ed60b8343c97d1173b993c658e6b5f91e9946ec8
jabir8210 <jabir8210@gmail.com>
2025-03-18 15:20:51 +0100

    increment 5

--- QSParsingConsoleApp.Test/QSParsingUnitTest.cs
+++ QSParsingConsoleApp.Test/QSParsingUnitTest.cs
@@ -60,8 +60,22 @@ public class QSParsingUnitTest
         Assert.AreEqual("John", result);
     }
 
+    [TestMethod]
+    public void ValueOf_MultiplePairs_ReturnsCorrectValue()
+    {
+        // Arrange
+        QSParser parser = new QSParser();
 
+        // Act
+        string resultName = parser.ValueOf("age=30&name=John&city=Aarhus", "name");
+        string resultAge = parser.ValueOf("age=30&name=John&city=Aarhus", "age");
+        string resultCity = parser.ValueOf("age=30&name=John&city=Aarhus", "city");
 
+        // Assert
+        Assert.AreEqual("John", resultName);  // Forventer "John" for "name"
+        Assert.AreEqual("30", resultAge);     // Forventer "30" for "age"
+        Assert.AreEqual("Aarhus", resultCity); // Forventer "Aarhus" for "city"
+    }
 
 
 }
